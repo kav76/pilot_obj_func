7c30468238ac9f829841f62c4a9ba97c594683b8
kav76 <121045568+kav76@users.noreply.github.com>
2023-02-08 22:08:34 +0300

    Init commit

---
+++ functions.js
@@ -0,0 +1,22 @@
+//
+//
+
+// Get pilot name function
+function get_pilot_name (pilot) {
+    return pilot.name;
+}
+
+// Get pilot age function
+function get_pilot_age (pilot) {
+    return pilot.age;
+}
+
+// Set pilot name function
+function set_pilot_name (pilot, name) {
+    pilot.name = name;
+}
+
+// Set pilot age function
+function set_pilot_age (pilot, age) {
+    pilot.age = age;
+}
